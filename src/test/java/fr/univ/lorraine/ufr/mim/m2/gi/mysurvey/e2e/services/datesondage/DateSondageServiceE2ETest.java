aa464ba266d59e0950a3fb1106d15b129c5b1251
Tim <tim.branstett@gmail.com>
2024-01-15 23:01:12 +0100

    e2e : CRUD Participant coverage

--- src/test/java/fr/univ/lorraine/ufr/mim/m2/gi/mysurvey/e2e/services/datesondage/DateSondageServiceE2ETest.java
+++
@@ -1,4 +0,0 @@
-package fr.univ.lorraine.ufr.mim.m2.gi.mysurvey.e2e.services.datesondage;
-
-public class DateSondageServiceE2ETest {
-}
--- src/test/java/fr/univ/lorraine/ufr/mim/m2/gi/mysurvey/e2e/services/sondage/SondageServiceE2ETest.java
+++
@@ -1,4 +0,0 @@
-package fr.univ.lorraine.ufr.mim.m2.gi.mysurvey.e2e.services.sondage;
-
-public class SondageServiceE2ETest {
-}
